9a75b9856e3e490ff9d6fa0027c1d598f5d79ddb
Catalin Halic <catalin.halic@gmail.com>
2023-05-30 20:11:17 +0300

    Add regex and varargs

---
+++ src/com/sda/regex/Main.java
@@ -0,0 +1,19 @@
+package com.sda.regex;
+
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        String textRegex = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$";
+        String email = "test@test.com";
+
+        boolean isMatched = true;
+
+        isMatched = Pattern
+                .compile(textRegex, Pattern.CASE_INSENSITIVE) // incarca regexul in memorie
+                .matcher(email) // incarca textul pe care dorim sa-l verificam
+                .matches(); // verifica textul si returneaza true / false
+
+        System.out.println(isMatched);
+    }
+}
